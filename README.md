e1a0488e041454960171cfce6c158ff068682781
Jyotiraditya <Jyotiraditya182@gmail.com>
2018-12-04 17:46:32 +0530

    Use telegram-send command

---
+++ README.md
@@ -0,0 +1,19 @@
+## Personal Scripts
+
+```bash
+gcpsetup.sh : To setup Google cloud Platform for Android Rom Developing
+```
+
+```bash
+[telegram]
+token =    # Add Bot token here
+chat_id =  # Add Chat id
+```
+## Telegram-Send
+
+To install this follow this:
+```bash
+$ sudo apt-get install python-pip
+$ sudo pip install telegram-send
+```
+For Further information Goto [Here](https://pypi.org/project/telegram-send).
--- aoscp.sh
+++ aoscp.sh
@@ -3,6 +3,9 @@
 # ccache
 export USE_CCACHE=1
 
+# Telegram
+export TG=~/BuildScripts/telegram.conf
+
 # Google Drive for Linux
 export GDRIVE=/usr/bin/gdrive
 
@@ -20,36 +23,32 @@ export BUILDDATE=$(date +%Y%m%d)
 export BUILDTIME=$(date +%H%M)
 
 # Repo sync
-echo "Starting repo sync."
 repo sync -f --force-sync --no-tags --no-clone-bundle -c
-echo "repo sync finished."
-
 # envsetup
-echo "Establishing build environment..."
 source build/envsetup.sh
-echo "Established"
-
 # lunch
-echo "Lunching Now"
 lunch $TARGET
-
 # Build
-echo "Starting build..."
+telegram-send --config $TG --format html "
+Repo syncing Done with <code>repo sync -f --force-sync --no-tags --no-clone-bundle -c</code>
+Established Build Enviroment...
+Lunched target
+"
 time mka $MAKETARGET -j$(nproc --all) | tee build.log
 
 EXITCODE=$?
 if [ $EXITCODE -eq 0 ];
 then
-	echo " Build finished successfully"
+	telegram-send --config $TG --format html " Build finished successfully"
 else
-	echo " Build Failed"
+	telegram-send --config $TG --format html " Build Failed"
 fi
 
 # Move zip to ROMs Folder
 mv $OUT/aoscp_santoni-$AOSCP_VERSION-$BUILDDATE-$AOSCP_BUILDTYPE.zip /home/Kakashi/ROMs/aoscp_santoni-$AOSCP_VERSION-$BUILDDATE-$AOSCP_BUILDTYPE.zip
 
 # Starting upload!
-echo "Uploading to Google Drive..."
+telegram-send --config $TG --format html "Uploading to Google Drive..."
 gdrive upload ~/ROMs/aoscp_santoni-$AOSCP_VERSION-$BUILDDATE-$AOSCP_BUILDTYPE.zip | tee -a /tmp/gdrive-$BUILDDATE-$BUILDTIME
 FILEID=$(cat /tmp/gdrive-$BUILDDATE-$BUILDTIME | tail -n 1 | awk '{ print $2 }')
 gdrive share $FILEID
@@ -58,4 +57,4 @@ MD5=$(cat /tmp/gdrive-info-$BUILDDATE-$BUILDTIME | grep 'Md5sum' | awk '{ print 
 NAME=$(cat /tmp/gdrive-info-$BUILDDATE-$BUILDTIME | grep 'Name' | awk '{ print $2 }')
 SIZE=$(cat /tmp/gdrive-info-$BUILDDATE-$BUILDTIME | grep 'Size' | awk '{ print $2 }')
 DLURL=$(cat /tmp/gdrive-info-$BUILDDATE-$BUILDTIME | grep 'DownloadUrl' | awk '{ print $2 }')
-echo -e "\nID: <code>$FILEID</code>\nPackage name: <code>$NAME</code>\nSize: <code>$SIZE</code>MB\nmd5sum: <code>$MD5</code>\nDownload link: $DLURL"
+echo -e "\nID: <code>$FILEID</code>\nPackage name: <code>$NAME</code>\nSize: <code>$SIZE</code>MB\nmd5sum: <code>$MD5</code>\nDownload link: $DLURL" | telegram-send --config $ROL --format html --stdin
