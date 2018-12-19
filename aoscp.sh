d52ef65f39c15c950c5100deb353d357aeb9ea53
Jyotiraditya <Jyotiraditya182@gmail.com>
2018-12-20 05:28:44 +0530

    BuildScripts: CleanUp the script
    * Given proper permissions in order for it to execute properly
    
    Signed-off-by: Jyotiraditya <Jyotiraditya182@gmail.com>

--- aoscp.sh
+++ aoscp.sh
@@ -9,55 +9,65 @@ export TG=~/BuildScripts/telegram.conf
 # Google Drive for Linux
 export GDRIVE=/usr/bin/gdrive
 
-# Switch to source directory
-cd ~/CypherOS
-
-# Log
-rm -rf log*.txt
-export LOGFILE=log-$BUILDDATE-$BUILDTIME.txt
-
-# Configs Needed
+# Build related
 export TARGET=aoscp_santoni-userdebug
 export AOSCP_VERSION=7.0.0
 export MAKETARGET=bacon
 export AOSCP_BUILDTYPE=unofficial
 
+# Switch to source directory
+cd ~/CypherOS
+
+# Delete Old Logs
+telegram-send --config $TG --format html "Deleting old logs now"
+rm -rf log*.txt
+
 # Date and time
 export BUILDDATE=$(date +%Y%m%d)
 export BUILDTIME=$(date +%H%M)
 
+# Log
+telegram-send --config $TG --format html "Logging to file <code>log-$BUILDDATE-$BUILDTIME.txt</code>"
+export LOGFILE=log-$BUILDDATE-$BUILDTIME.txt
+
 # Repo sync
+telegram-send --config $TG --format html "Starting repo sync. Executing command: <code>repo sync -f --force-sync --no-tags --no-clone-bundle -c</code>"
 repo sync -f --force-sync --no-tags --no-clone-bundle -c
+telegram-send --config $TG --format html "repo sync finished."
 
 # envsetup
+telegram-send --config $TG --format html "Establishing build environment..."
 source build/envsetup.sh
 
 # lunch
+telegram-send --config $TG --format html "Starting lunch... Lunching <code>$DEVICENAME</code>"
 lunch $TARGET
 
+# installclean
+telegram-send --config $TG --format html "Removing out/ directory..."
+rm -rf out/
+
 # Build
-telegram-send --config $TG --format html "
-Repo syncing Done with <code>repo sync -f --force-sync --no-tags --no-clone-bundle -c</code>
-Established Build Enviroment...
-Lunched target
-Deleted old logs and creating new
-name : <code>log-$BUILDDATE-$BUILDTIME.txt</code>
-"
+telegram-send --config $TG --format html "Starting build... Building target <code>$MAKETARGET</code>"
 time mka $MAKETARGET -j$(nproc --all) > ./$LOGFILE &
+# LAUNCH PROGRESS OBSERVER
+sleep 60
 while test ! -z "$(pidof soong_ui)"; do
-        sleep 300
+        # Go away for 10 mins
+        sleep 120
+        # bot: *WAkES uP, triggered*
+        # Get latest percentage
         PERCENTAGE=$(cat $LOGFILE | tail -n 1 | awk '{ print $2 }')
-        telegram-send --config $TG --format html "Current percentage: <code>$PERCENTAGE</code>";
+        # REPORT PerCentage to that damn TeLeGraM
+        telegram-send --config $TG --format html "Current percentage: $PERCENTAGE";
 done
 EXITCODE=$?
-if [ $EXITCODE -eq 0 ];
-then
-	telegram-send --config $TG --format html " Build finished successfully";
-else
-	telegram-send --config $TG --format html " Build Failed";
-	telegram-send --config $TG --file $LOGFILE;
+if [ $EXITCODE -ne 0 ];
+	then telegram-send --config $TG --format html "Build failed! Check log file <code>$LOGFILE</code>";
+	     telegram-send --config $TG --file $LOGFILE;
 	exit 1;
 fi
+telegram-send --config TG --format html "Build finished successfully! Uploading new build..."
 
 # Move zip to ROMs Folder
 mv $OUT/aoscp_santoni-$AOSCP_VERSION-$BUILDDATE-$AOSCP_BUILDTYPE.zip /home/Kakashi/ROMs/aoscp_santoni-$AOSCP_VERSION-$BUILDDATE-$AOSCP_BUILDTYPE.zip
